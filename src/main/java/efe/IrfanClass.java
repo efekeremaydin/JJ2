840b3a79548214382d04592b90674f5fa6a1cc85
Irfan Tursun <irfan2rsun@gmail.com>
2024-02-23 13:01:49 -0800

    Added IrfanClass

---
+++ src/main/java/efe/IrfanClass.java
@@ -0,0 +1,9 @@
+package efe;
+
+public class IrfanClass {
+    public static void main(String[] args) {
+        System.out.println("Lambda Rules\n" +
+                "1- Functional interface\n" + "2- Matching Implementations");
+    }
+
+}
